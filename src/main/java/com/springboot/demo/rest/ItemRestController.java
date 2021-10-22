a2160bcce027ccb10b77fdeef351190db57be74a
PrashantKolhe <kolheprashant204@gmail.com>
2021-10-21 21:53:47 -0700

    Injecting custom application properties Rename changes

--- src/main/java/com/springboot/demo/rest/ItemRestController.java
+++ src/main/java/com/springboot/demo/rest/ItemRestController.java
@@ -7,13 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FunRestController {
-		
+public class ItemRestController {
+	@Value("${item.name}")
+	private String itemName;
+
+	@Value("${item.quantity}")
+	private String itemQuantity;
+
+	@GetMapping("/item")
+	public String getItemInfo(){
+		return "itemName= " + itemName +
+				", itemQuantity= " + itemQuantity;
+	}
+
 	// expose "/" that return "Hello World"
 	
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello World! Time on server issssss123 " + LocalDateTime.now();
+		return "Hello World! Time on server is " + LocalDateTime.now();
 	}
 	
 }
